15372764ae1c0ef3f28fe906728f26eb241b89e2
naonao <naonao@xizaideMacBook-Pro.local>
2017-10-21 21:02:27 +0800

    dev

---
+++ smaug_cloud_data/src/main/java/smaug/cloud/data/mapper/queue/QueuingEntityMapper.java
@@ -0,0 +1,17 @@
+package smaug.cloud.data.mapper.queue;
+
+import smaug.cloud.data.entity.queue.QueuingEntity;
+
+public interface QueuingEntityMapper {
+    int deleteByPrimaryKey(Long serialid);
+
+    int insert(QueuingEntity record);
+
+    int insertSelective(QueuingEntity record);
+
+    QueuingEntity selectByPrimaryKey(Long serialid);
+
+    int updateByPrimaryKeySelective(QueuingEntity record);
+
+    int updateByPrimaryKey(QueuingEntity record);
+}
