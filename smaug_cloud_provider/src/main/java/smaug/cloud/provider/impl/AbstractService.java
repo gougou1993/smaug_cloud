4454eb6a3a5c8cfff7a5e211a017fad4841d4254
naonao <naonao@xizaideMacBook-Pro.local>
2017-11-19 11:55:10 +0800

    add mq

--- smaug_cloud_provider/src/main/java/smaug/cloud/provider/impl/AbstractService.java
+++ smaug_cloud_provider/src/main/java/smaug/cloud/provider/impl/AbstractService.java
@@ -1,10 +1,10 @@
 package smaug.cloud.provider.impl;
 
-import com.netflix.discovery.converters.Auto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import smaug.cloud.common.consts.MQConst;
+import smaug.cloud.common.utils.cache.JedisUtil;
 import smaug.cloud.common.utils.cache.SmaugJedisUtil;
 import smaug.cloud.common.utils.jsons.FastJsonUtil;
 import smaug.cloud.common.utils.mq.SmaugMessageUtil;
@@ -16,9 +16,8 @@ import javax.annotation.Resource;
  */
 public class AbstractService {
 
-
     @Resource(name = "smaugJedisUtil")
-    protected SmaugJedisUtil smaugJedisUtil;
+    protected JedisUtil smaugJedisUtil;
 
     protected FastJsonUtil jsonUtil = new FastJsonUtil();
 
--- smaug_cloud_provider/src/main/java/smaug/cloud/provider/impl/TestServiceImpl.java
+++ smaug_cloud_provider/src/main/java/smaug/cloud/provider/impl/TestServiceImpl.java
@@ -1,19 +1,14 @@
 package smaug.cloud.provider.impl;
 
 
-import org.apache.activemq.command.ActiveMQQueue;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import smaug.cloud.api.interfaces.TestService;
 import smaug.cloud.api.vos.article.ArticleResponse;
 import smaug.cloud.api.vos.queue.Queuing;
 import smaug.cloud.api.vos.user.UserResponse;
-import smaug.cloud.common.consts.MQConst;
-import smaug.cloud.common.utils.mq.ActiveMQProducer;
 import smaug.cloud.data.entity.article.ArticleEntity;
 import smaug.cloud.data.entity.demo.UserEntity;
-import javax.jms.Destination;
-import javax.jms.Queue;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -24,13 +19,11 @@ import java.util.List;
 @Service("testServiceImpl")
 public class TestServiceImpl extends AbstractService implements TestService {
 
-    @Autowired
-    private ActiveMQProducer activeMQProducer;
 
     @Override
     public String test() {
-        commonMessageUtil.convertAndSend("wowoxiangdemandaren");
-        return mqConst.getSmaugCommonQueue();
+        commonMessageUtil.convertAndSend("胖云是笨蛋");
+        return "succ";
     }
 
     @Override
